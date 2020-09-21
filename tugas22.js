b4c982856425fe4ba91c678946c8f5e55bbbdd2d
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 15:37:10 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+var kalimat = ["Saya", "ingin", "belajar", "bersama"]
+
+function panggilForEach(){
+	kalimat.forEach(function(item,index){
+		console.log("Item : ",(item)," index ke ",(index))
+	})
+}
+panggilForEach()
